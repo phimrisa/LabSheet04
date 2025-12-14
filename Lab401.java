773f1d264ff5255abf7851dc40b6a4ad19de1673
phimrisa <phimrisa@gmail.com>
2025-12-14 16:19:48 +0700

    Update Lab401.java

--- Lab401.java
+++ Lab401.java
@@ -4,6 +4,14 @@ public class Lab401 {
     public static void main(String[] args) {
         String fullname = JOptionPane.showInputDialog("Enter your fullname:");
 
+        int index = fullname.indexOf(" ");
 
+        while (fullname.trim().indexOf(" ") == -1) {
+            fullname = JOptionPane.showInputDialog("Enter your fullname:");
+        }
+
+        JOptionPane.showMessageDialog(null,
+                "Welcome, " + fullname.toUpperCase().charAt(0) +
+                        fullname.substring(1, index) + " " + fullname.toUpperCase().substring(index + 1, fullname.length() - 1));
     }
 }
