9569badbb3ff84f353b211943e6489e0cbd0113c
phimrisa <phimrisa@gmail.com>
2025-12-14 16:22:51 +0700

    Update Lab405.java

--- Lab405.java
+++ Lab405.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Lab405 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Input some sentence: ");
+        String sentence = scanner.nextLine();
+
+        while(!sentence.endsWith(".")){
+            System.out.print("The sentence must end with full stop point: ");
+            sentence = scanner.nextLine();
+        }
+
+        System.out.println();
+
+        String[] split_sentence = sentence.split(" ");
+        for(int i = 0; i <= split_sentence.length; i++){
+            System.out.println(split_sentence[i]);
+        }
     }
 }
